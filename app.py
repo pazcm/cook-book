8c51e15ad716525b44f1672022f5d03975a96f48
Ubuntu <ubuntu@ip-172-31-87-135.ec2.internal>
2019-07-18 12:07:00 +0000

    connect Flask to MongoDB Atlas

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cook-book-db'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster2-6phdr.mongodb.net/cook-book-db?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello Cook Book!'
+@app.route('/get_recipes')
+def get_recipe():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', '0.0.0.0'),
-            port=int(os.environ.get('PORT', '5000')),
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
